e24ec9a3ab1b0da3615c5a6179183b1ecd4a0eb6
vijay2999 <vijaydisale010@gmail.com>
2022-10-17 23:55:57 +0530

    assignment complet

--- src/routes/route.js
+++ src/routes/route.js
@@ -8,11 +8,7 @@ router.get('/test-me', function (req, res) {
     res.send('My second ever api!')
 });
 
-router.get('/students', function (req, res){
-    console.log("The path params in the request are : ", req.params)
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
-})
+
 
 
 // Example 1 for path params
@@ -39,5 +35,90 @@ router.get('/student-details/:name', function(req, res){
     console.log('Name of the student is ', studentName)
     res.send('Dummy response')
 })
+//Assignment problem statement 1
 
+router.get('/movies', function (req, res){
+    console.log("The path params in the request are : ", req.params)
+    let movies = ['Rang de Basanti', 'the shining', 'Lord of Rings','Batman begins']
+    res.send(movies)
+})
+
+
+
+//Assignment problem statement 2 && 3
+
+ router.get('/movies/:indexNumber',function(req,res){
+
+    let movies = ['Rang de Basanti', 'the shining', 'Lord of Rings','Batman begins']
+     const index=req.params.indexNumber
+    const index1=(movies.length)
+    if(index>=index1){
+
+        res.send('use valid index')
+    }
+    else{
+    res.send(movies[index])
+    }
+       //res.send(movies[index])
+ })
+
+//Assignment problem statement 4
+
+router.get('/films/:movie_object', function(req,res){
+
+    
+    const arr=[ {
+    'id': 1,
+        'name': 
+        'The Shining'
+       }, {
+        'id': 2,
+        'name': 'Incendies'
+       }, {
+        'id': 3,
+        'name': 'Rang de Basanti'
+       }, {
+        'id': 4,
+        'name': 'Finding Nemo'
+       }]
+
+       res.send(arr)
+       
+    
+})
+
+//Assignment problem statement 5
+
+router.get('/films1/:filmid', function(req,res){
+
+    const arr=[ {
+        'id': 1,
+            'name': 
+            'The Shining'
+           }, {
+            'id': 2,
+            'name': 'Incendies'
+           }, {
+            'id': 3,
+            'name': 'Rang de Basanti'
+           }, {
+            'id': 4,
+            'name': 'Finding Nemo'
+           }] 
+
+           const index=req.params.filmid
+
+           const sizeOfarr=(arr.length)
+
+           if(index>sizeOfarr ||  index==0){
+            
+            res.send('No movie exist with this id')
+           }
+           else{
+
+            const index1=req.params.filmid-1
+            
+            res.send(arr[index1])
+           }
+})
 module.exports = router;
