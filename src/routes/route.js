7f3f1bfddd78a8e46eb383a50bb830b884790b66
vijay2999 <vijaydisale010@gmail.com>
2022-10-18 14:16:08 +0530

    code added

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,43 +2,43 @@ const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    res.send('My second ever api!')
-});
+// router.get('/test-me', function (req, res) {
+//     console.log('My batch is', abc.name)
+//     abc.printName()
+//     res.send('My second ever api!')
+// });
 
 
 
 
-// Example 1 for path params
-router.get('/students/:studentName', function(req, res){
-    // ':' denotes that the following part of route is a variable
-    // The value of this variable is what we are sending in the request url after /students
-    // This value is set in the form of an object inside req.params
-    // The object contain key value pairs
-    // key is the variable in the route
-    // value is whatever dynamic value sent in the request url
-    let myParams = req.params
+// // Example 1 for path params
+// router.get('/students/:studentName', function(req, res){
+//     // ':' denotes that the following part of route is a variable
+//     // The value of this variable is what we are sending in the request url after /students
+//     // This value is set in the form of an object inside req.params
+//     // The object contain key value pairs
+//     // key is the variable in the route
+//     // value is whatever dynamic value sent in the request url
+//     let myParams = req.params
 
-    // params attribute is fixed in a request object
-    // params contains the path parameters object
-    console.log("The path params in the request are : ", myParams)
-    res.send('The full name is ' + myParams.studentName )
-})
+//     // params attribute is fixed in a request object
+//     // params contains the path parameters object
+//     console.log("The path params in the request are : ", myParams)
+//     res.send('The full name is ' + myParams.studentName )
+// })
 
-// Example 2 for path params
-router.get('/student-details/:name', function(req, res){
-    let requestParams = req.params
-    console.log("This is the request ", requestParams)
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
-    res.send('Dummy response')
-})
+// // Example 2 for path params
+// router.get('/student-details/:name', function(req, res){
+//     let requestParams = req.params
+//     console.log("This is the request ", requestParams)
+//     let studentName = requestParams.name
+//     console.log('Name of the student is ', studentName)
+//     res.send('Dummy response')
+// })
 //Assignment problem statement 1
 
 router.get('/movies', function (req, res){
-    console.log("The path params in the request are : ", req.params)
+    
     let movies = ['Rang de Basanti', 'the shining', 'Lord of Rings','Batman begins']
     res.send(movies)
 })
@@ -91,7 +91,7 @@ router.get('/films/:movie_object', function(req,res){
 
 router.get('/films1/:filmid', function(req,res){
 
-    const arr=[ {
+    const arr=[{
         'id': 1,
             'name': 
             'The Shining'
@@ -110,7 +110,7 @@ router.get('/films1/:filmid', function(req,res){
 
            const sizeOfarr=(arr.length)
 
-           if(index>sizeOfarr ||  index==0){
+           if(index>sizeOfarr ||  index<=0){
             
             res.send('No movie exist with this id')
            }
