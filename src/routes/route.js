accf36493413a919315dcb3d577213dded17091d
vijay2999 <vijaydisale010@gmail.com>
2022-10-20 11:13:14 +0530

    post api assignment completed

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,123 +2,83 @@ const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
 
-// router.get('/test-me', function (req, res) {
-//     console.log('My batch is', abc.name)
-//     abc.printName()
-//     res.send('My second ever api!')
-// });
+let arr =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ]
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ]
+       },
+   ]
 
+router.post('/test-post', function(req,res){
 
-
-
-// // Example 1 for path params
-// router.get('/students/:studentName', function(req, res){
-//     // ':' denotes that the following part of route is a variable
-//     // The value of this variable is what we are sending in the request url after /students
-//     // This value is set in the form of an object inside req.params
-//     // The object contain key value pairs
-//     // key is the variable in the route
-//     // value is whatever dynamic value sent in the request url
-//     let myParams = req.params
-
-//     // params attribute is fixed in a request object
-//     // params contains the path parameters object
-//     console.log("The path params in the request are : ", myParams)
-//     res.send('The full name is ' + myParams.studentName )
-// })
-
-// // Example 2 for path params
-// router.get('/student-details/:name', function(req, res){
-//     let requestParams = req.params
-//     console.log("This is the request ", requestParams)
-//     let studentName = requestParams.name
-//     console.log('Name of the student is ', studentName)
-//     res.send('Dummy response')
-// })
-//Assignment problem statement 1
-
-router.get('/movies', function (req, res){
+    let info=req.body
+    let bodynames=info.name
     
-    let movies = ['Rang de Basanti', 'the shining', 'Lord of Rings','Batman begins']
-    res.send(movies)
+     for(i=0;i<arr.length;i++){
+
+        if(bodynames==arr[i].name){
+
+            res.send("player already exist")
+        } }
+    arr.push(info)
+console.log(arr)
+res.send("detaile upgraded")
 })
 
+router.get('/exe1', function(req,res){
 
+    arr=[1,2,3,5,6,7,8]
 
-//Assignment problem statement 2 && 3
+let x=arr.length+1
 
- router.get('/movies/:indexNumber',function(req,res){
+let sumofallno= x*(x+1)/2
 
-    let movies = ['Rang de Basanti', 'the shining', 'Lord of Rings','Batman begins']
-     const index=req.params.indexNumber
-    const index1=(movies.length)
-    if(index>=index1){
+    let sum=0
+    for(i=0;i<arr.length;i++){
 
-        res.send('use valid index')
-    }
-    else{
-    res.send(movies[index])
-    }
-       //res.send(movies[index])
- })
+        sum=sum+arr[i]
+    }   
 
-//Assignment problem statement 4
-
-router.get('/films/:movie_object', function(req,res){
-
-    
-    const arr=[ {
-    'id': 1,
-        'name': 
-        'The Shining'
-       }, {
-        'id': 2,
-        'name': 'Incendies'
-       }, {
-        'id': 3,
-        'name': 'Rang de Basanti'
-       }, {
-        'id': 4,
-        'name': 'Finding Nemo'
-       }]
-
-       res.send(arr)
-       
-    
+result=sumofallno-sum
+console.log(result)
+res.send("first problem done")
 })
 
-//Assignment problem statement 5
-
-router.get('/films1/:filmid', function(req,res){
-
-    const arr=[{
-        'id': 1,
-            'name': 
-            'The Shining'
-           }, {
-            'id': 2,
-            'name': 'Incendies'
-           }, {
-            'id': 3,
-            'name': 'Rang de Basanti'
-           }, {
-            'id': 4,
-            'name': 'Finding Nemo'
-           }] 
-
-           const index=req.params.filmid
-
-           const sizeOfarr=(arr.length)
-
-           if(index>sizeOfarr ||  index<=0){
-            
-            res.send('No movie exist with this id')
-           }
-           else{
-
-            const index1=req.params.filmid-1
-            
-            res.send(arr[index1])
-           }
+router.get('/exe2', function(req,res){
+arr2=[33,34,35,37,38]
+let sum1=0
+for(i=0;i<arr2.length;i++){
+    sum1=sum1+arr2[i]
+}
+let ntr1=arr2.length+1
+let cbr=arr2.pop()
+let sumofallno1=(ntr1*(arr2[0]+cbr)/2)
+ result=sumofallno1-sum1
+ console.log(result)
+ res.send("second problem done")
 })
 module.exports = router;
