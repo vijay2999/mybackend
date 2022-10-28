ec71bb30bf729ae5b01cbde37a9a89fd8b046862
vijay2999 <vijaydisale010@gmail.com>
2022-10-28 15:11:00 +0530

    assign completed

---
+++ src/models/authorModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const authorSchema = new mongoose.Schema( {
+    author_id:String,
+    author_name:String,
+         age:Number,
+         address:String
+
+}, { timestamps: true });
+module.exports = mongoose.model('Book2', authorSchema)
